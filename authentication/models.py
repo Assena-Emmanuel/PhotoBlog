0819461cf83795fd7415118da7807596099cf69c
assena <assenaemmanuel3@gmail.com>
2024-06-21 14:43:46 +0100

    Création du model User personnalisé

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    CREATEUR = "CREATEUR"
+    ABONNE = "ABONNE"
+
+    ROLE_CHOIX = (
+        (CREATEUR, 'Créateur'),
+        (ABONNE, "Abonné")
+    )
+
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOIX)
